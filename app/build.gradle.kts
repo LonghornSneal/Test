e39d99b7475ddcfa96bdd557f22b0879febc9893
LonghornSneal <jlawhon107@gmail.com>
2025-12-08 05:42:10 -0600

    gfgf

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         applicationId = "com.cosmobond.watchface"
         minSdk = 34
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildTypes {
